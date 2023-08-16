7b1e731ee4f7aab178bf86e0c1843d059b466495
Steve Kircher <39098047+skircher@users.noreply.github.com>
2023-08-16 07:34:28 -0600

    Add JWT verification, call to payments, and dynamic request (#1)

---
+++ src/main/java/com/cybersource/example/service/PaymentsService.java
@@ -0,0 +1,45 @@
+package com.cybersource.example.service;
+
+import Api.PaymentsApi;
+import Invokers.ApiClient;
+import Invokers.ApiException;
+import Model.*;
+import com.cybersource.authsdk.core.ConfigException;
+import com.cybersource.authsdk.core.MerchantConfig;
+import com.cybersource.example.config.ApplicationProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentsService {
+    @Autowired
+    private final ApplicationProperties applicationProperties;
+
+    public String makePaymentWithTransientToken(final String transientToken) throws ConfigException, ApiException {
+        final ApiClient apiClient = new ApiClient(new MerchantConfig(applicationProperties.getAsJavaProperties()));
+        final PaymentsApi paymentApi = new PaymentsApi(apiClient);
+
+        final CreatePaymentRequest paymentRequest = createPaymentRequest(transientToken);
+        final PtsV2PaymentsPost201Response response = paymentApi.createPayment(paymentRequest);
+
+        return response.toString();
+    }
+
+    private static CreatePaymentRequest createPaymentRequest(final String transientToken) {
+        // Additional fields like billTo, shipTo, amountDetails can be provided here to override what Unified Checkout collected
+        final Ptsv2paymentsClientReferenceInformation clientReferenceInfo =
+                new Ptsv2paymentsClientReferenceInformation().code("test_payment");
+        final Ptsv2paymentsTokenInformation tokenInformation = new Ptsv2paymentsTokenInformation().transientTokenJwt(transientToken);
+        return new CreatePaymentRequest()
+                .clientReferenceInformation(clientReferenceInfo)
+                .tokenInformation(tokenInformation)
+                // TODO: Adding these in case $.captureMandate.billingType = PARTIAL or NONE,
+                //  or $.captureMandate.requestShipping = false. Long term, this should be fixed and this shouldn't be necessary.
+                .orderInformation(new Ptsv2paymentsOrderInformation()
+                        .billTo(new Ptsv2paymentsOrderInformationBillTo().administrativeArea("NY"))
+                        .shipTo(new Ptsv2paymentsOrderInformationShipTo().administrativeArea("NY"))
+                );
+    }
+}
--- src/main/java/com/cybersource/example/web/UnifiedCheckoutController.java
+++ src/main/java/com/cybersource/example/web/UnifiedCheckoutController.java
@@ -1,25 +1,31 @@
 package com.cybersource.example.web;
 
-import com.cybersource.example.domain.TransientTokenRequest;
+import Model.GenerateUnifiedCheckoutCaptureContextRequest;
 import com.cybersource.example.service.CaptureContextService;
-import com.cybersource.example.service.JwtDecoderService;
+import com.cybersource.example.service.JwtProcessorService;
+import com.cybersource.example.service.PaymentsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.nio.file.Files;
 import java.util.stream.Stream;
 
-import static com.cybersource.example.util.RequestBuilderUtils.getRequestJsonAsString;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Controller
 @RequiredArgsConstructor
-@SessionAttributes({"jwt", "bootstrapVersion", "unifiedCheckoutLibraryVersion"})
+@SessionAttributes({"captureContextJwt", "bootstrapVersion", "unifiedCheckoutLibraryVersion", "encodedTransientToken"})
 public class UnifiedCheckoutController {
 
     @Value("classpath:capture-context-request.json")
@@ -30,7 +36,10 @@ public class UnifiedCheckoutController {
     private final CaptureContextService captureContextService;
 
     @Autowired
-    private final JwtDecoderService jwtDecoderService;
+    private final JwtProcessorService jwtProcessorService;
+
+    @Autowired
+    private final PaymentsService paymentsService;
 
     @GetMapping("/")
     @SneakyThrows
@@ -39,16 +48,31 @@ public class UnifiedCheckoutController {
             final long lineCount = lines.count();
             model.addAttribute("requestLineCount", lineCount);
         }
-        model.addAttribute("requestJson", getRequestJsonAsString(captureContextRequestJson));
+        model.addAttribute("requestJson", IOUtils.toString(captureContextRequestJson.getInputStream(), UTF_8));
         model.addAttribute("bootstrapVersion", BOOTSTRAP_VERSION);
         return "index";
     }
 
     @PostMapping("/capture-context")
-    public String requestCaptureContext(final Model model) {
-        final String jwt = captureContextService.requestCaptureContextUsingSDK();
-        model.addAttribute("jwt", jwt);
-        model.addAttribute("unifiedCheckoutLibraryVersion", jwtDecoderService.getUnifiedCheckoutLibraryVersion(jwt));
+    @SneakyThrows
+    public String requestCaptureContext(final @RequestParam("captureContextRequest") String captureContextRequest,
+                                        final Model model) {
+        final GenerateUnifiedCheckoutCaptureContextRequest mappedRequest = new ObjectMapper()
+                .readValue(captureContextRequest.getBytes(UTF_8), GenerateUnifiedCheckoutCaptureContextRequest.class);
+
+        // Use the SDK to get the capture context using our mapped request
+        final String jwt = captureContextService.requestCaptureContextUsingSDK(mappedRequest);
+        // Or uncomment the line below and comment the line above to test without using the SDK
+        // final String jwt = captureContextService.requestCaptureContextWithoutSDK(captureContextRequest);
+
+        // Verify that the response is from CyberSource
+        final String decodedBody = jwtProcessorService.verifyJwtAndGetDecodedBody(jwt);
+        // Dynamically retrieve the client library version to pass back to our frontend
+        final String clientVersion = jwtProcessorService.getClientVersionFromDecodedBody(decodedBody);
+
+        model.addAttribute("captureContextJwt", jwt);
+        model.addAttribute("decodedBody", new ObjectMapper().readTree(decodedBody).toPrettyString());
+        model.addAttribute("unifiedCheckoutLibraryVersion", clientVersion);
         return "capture-context";
     }
 
@@ -58,8 +82,21 @@ public class UnifiedCheckoutController {
     }
 
     @PostMapping("/token")
-    public String viewToken(final @RequestParam TransientTokenRequest transientToken, final Model model){
-        model.addAttribute("transientToken", transientToken.token());
+    @SneakyThrows
+    public String viewToken(final @RequestParam String transientToken, final Model model) {
+        final String decodedBody = jwtProcessorService.verifyJwtAndGetDecodedBody(transientToken);
+        model.addAttribute("encodedTransientToken", transientToken);
+        model.addAttribute("decodedTransientToken", new ObjectMapper().readTree(decodedBody).toPrettyString());
         return "token";
+    }
+
+    @PostMapping("/receipt")
+    @SneakyThrows
+    public String receipt(final Model model) {
+        // Call the payments endpoint to make a request using our transient token
+        final String paymentsResponse = paymentsService.makePaymentWithTransientToken(
+                model.getAttribute("encodedTransientToken").toString());
+        model.addAttribute("paymentsResponse", paymentsResponse);
+        return "receipt";
     }
 }
